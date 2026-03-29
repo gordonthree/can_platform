0bbc4293c8422a72a473c15465f53d44d1c098b6
Gordon McLellan <gordonthree@gmail.com>
2026-03-29 00:31:12 -0400

    make can_msg_t more available

--- src/can_platform.h
+++ src/can_platform.h
@@ -2,24 +2,26 @@
 #include <string.h>             // for memset
 #include "canbus_project.h"     // for nodeInfo_t and subModule_t
 
-#ifdef ESP32
-    #include "driver/twai.h"
-    typedef twai_message_t can_msg_t;
-#else
-    typedef struct {
-        uint32_t identifier;
-        uint8_t  data_length_code;
-        uint8_t  data[8];
-    } can_msg_t;
-#endif
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 
+typedef struct {
+    uint32_t identifier;
+    uint8_t  data_length_code;
+    uint8_t  data[8];
+} can_msg_t;
+
+
+#ifdef ESP32
+#include "driver/twai.h"
+/* TX queue payload type */
+typedef twai_message_t CanTxMsg_t;
+
 can_msg_t toCanMsg(const twai_message_t *msg);
 twai_message_t toTwaiMsg(const can_msg_t *msg);
+#endif
 
 #ifdef __cplusplus
 }
